1b06d38c0d5552f5798b63f67b4ac25a8a1ff4af
NgChiseng <ngchiseng@gmail.com>
2017-06-27 14:18:35 -0400

    TPA-01 #comment Fixed some warning.

--- app/src/main/java/com/idbc/ngchiseng/timepieceapp/TimePieceSharedPreferences.java
+++ app/src/main/java/com/idbc/ngchiseng/timepieceapp/TimePieceSharedPreferences.java
@@ -11,18 +11,18 @@ import android.preference.PreferenceManager;
  * only in the first time that the application is executed.
  */
 
-public class TimePieceSharedPreferences {
+class TimePieceSharedPreferences {
 
     /*
         Static Keys of each SharedPreferences
     */
-    static final String EXECUTED = "executed";
-    static final String AUTH_TOKEN= "auth_token";
+    private static final String EXECUTED = "executed";
+    private static final String AUTH_TOKEN= "auth_token";
 
     /* This define the default SharedPreference that will return in case of error. It is necessary
     for implements SharedPreference in a Class.
      */
-    static SharedPreferences getSharedPreferences(Context ctx) {
+    private static SharedPreferences getSharedPreferences(Context ctx) {
         return PreferenceManager.getDefaultSharedPreferences(ctx);
     }
 
@@ -32,7 +32,7 @@ public class TimePieceSharedPreferences {
         @param [Context] ctx Context of the Activity that call the corresponding method.
         @return [Value]
     */
-    public static Boolean getExecuted(Context ctx) {
+    private static Boolean getExecuted(Context ctx) {
         return getSharedPreferences(ctx).getBoolean(EXECUTED, false);
     }
 
@@ -43,7 +43,7 @@ public class TimePieceSharedPreferences {
         @param [Type_Value] <Value> Value belonging to each SharedPreferences.
         @return [Void]
     */
-    public static void setExecuted(Context ctx, Boolean value) {
+    private static void setExecuted(Context ctx, Boolean value) {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.putBoolean(EXECUTED, value);
         editor.apply();
@@ -58,6 +58,6 @@ public class TimePieceSharedPreferences {
     public static void clearSharedPreferences(Context ctx) {
         SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
         editor.clear();
-        editor.commit();
+        editor.apply();
     }
 }
